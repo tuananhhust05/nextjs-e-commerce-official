8dd2bac3c7752eb23bd72cc2d0f7a9b22a81f912
tuananhhust05 <tuananhhust05@gmail.com>
2023-02-28 09:12:36 +0700

    update

--- components/Layout.js
+++ components/Layout.js
@@ -69,18 +69,6 @@ export default function Layout({ title, children }) {
                     <SearchIcon className="h-5 w-5"></SearchIcon>
                   </button>
               </form>
-              <Link href="/cart">
-                <a className="p-2">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-                </svg>
-                  {cartItemsCount > 0 && (
-                    <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
-                      {cartItemsCount}
-                    </span>
-                  )}
-                </a>
-              </Link>
               {status === 'loading' ? (
                 'Loading'
               ) : session?.user ? (
@@ -128,6 +116,18 @@ export default function Layout({ title, children }) {
                   <a className="p-2">Login</a>
                 </Link>
               )}
+              <Link href="/cart">
+                <a className="p-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                </svg>
+                  {cartItemsCount > 0 && (
+                    <span className={ `ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white ${styles.count_product}`}>
+                      {cartItemsCount}
+                    </span>
+                  )}
+                </a>
+              </Link>
             </div>
           </nav>
         </header>
